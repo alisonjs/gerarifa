2b7525ef3af31e85673db78f5d035d9a10bda25a
Alison Educ <alisoneduc@pop-os.localdomain>
2021-06-06 21:07:22 -0300

    Initial Commit

---
+++ src/main/java/com/gerarifa/Gerarifa/GerarifaApplication.java
@@ -0,0 +1,13 @@
+package com.gerarifa.Gerarifa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerarifaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerarifaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gerarifa/Gerarifa/GerarifaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerarifa.Gerarifa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerarifaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
